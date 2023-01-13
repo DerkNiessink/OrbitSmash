69bf48c14a03c1bc98cbf7cf8e37857c13c19f8c
KatoSchmidt <70628138+KatoSchmidt@users.noreply.github.com>
2023-01-13 14:38:10 +0100

    even een extra kato bestand aangemaakt

---
+++ kato.py
@@ -0,0 +1,14 @@
+import numpy as np
+def octree(self, objects: list[Object], subsections = [6904.541, 6925.362, 7024.6, 7127.719, 7189.4, 7243.753, 7387.845, 8370.819], margin = 10):
+    
+    # a 10% margin of the subsections 
+    differance = np.diff(subsections) * 0.1
+    subclass = [i for i in range(8)]
+
+    for object in objects: 
+        
+        
+        
+        pass 
+    return
+octree()
