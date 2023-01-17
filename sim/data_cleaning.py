fced4ed5d37026e31c4215d0fb17c67f1d9f8f7c
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2023-01-17 19:07:08 +0100

    lots of changes

--- sim/data_cleaning.py
+++ sim/data_cleaning.py
@@ -1,41 +1,84 @@
 import pandas as pd
 import datetime
-import matplotlib.pyplot as plt
-import numpy as np 
+from scipy.spatial.transform import Rotation
 
-dataset = pd.read_csv('data/satellites.csv')
 
-# removing irrelevant columns 
-dataset = dataset.drop(columns=['CCSDS_OMM_VERS', 'COMMENT', 'CREATION_DATE', 'ORIGINATOR', 'OBJECT_NAME', 'OBJECT_ID', 'CENTER_NAME', 
-                        'REF_FRAME', 'TIME_SYSTEM', 'MEAN_ELEMENT_THEORY', 'EPHEMERIS_TYPE', 'CLASSIFICATION_TYPE', 'ELEMENT_SET_NO', 
-                        'REV_AT_EPOCH', 'BSTAR', 'MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT', 'SITE', 'DECAY_DATE', 'FILE', 'GP_ID', 
-                        'TLE_LINE0', 'TLE_LINE1', 'TLE_LINE2', 'ECCENTRICITY', 'MEAN_MOTION','PERIOD', 'APOAPSIS', 'PERIAPSIS',
-                        'COUNTRY_CODE' ])
+dataset = pd.read_csv("data/satellites.csv")
+
+# removing irrelevant columns
+dataset = dataset.drop(
+    columns=[
+        "CCSDS_OMM_VERS",
+        "COMMENT",
+        "CREATION_DATE",
+        "ORIGINATOR",
+        "OBJECT_NAME",
+        "OBJECT_ID",
+        "CENTER_NAME",
+        "REF_FRAME",
+        "TIME_SYSTEM",
+        "MEAN_ELEMENT_THEORY",
+        "EPHEMERIS_TYPE",
+        "CLASSIFICATION_TYPE",
+        "ELEMENT_SET_NO",
+        "REV_AT_EPOCH",
+        "BSTAR",
+        "MEAN_MOTION_DOT",
+        "MEAN_MOTION_DDOT",
+        "SITE",
+        "DECAY_DATE",
+        "FILE",
+        "GP_ID",
+        "TLE_LINE0",
+        "TLE_LINE1",
+        "TLE_LINE2",
+        "ECCENTRICITY",
+        "MEAN_MOTION",
+        "PERIOD",
+        "APOAPSIS",
+        "PERIAPSIS",
+        "COUNTRY_CODE",
+    ]
+)
+
 
 def epoch(df_column):
-    date_list = list(df_column) 
+    date_list = list(df_column)
     new_date_list = []
 
     for data in date_list:
-        date, time = data.split('T')
-        year, month, day = date.split('-')
-        hour, minute, second = time.split(':')
+        date, time = data.split("T")
+        year, month, day = date.split("-")
+        hour, minute, second = time.split(":")
         second = second[0:2]
 
-        new_date_list.append(datetime.datetime(int(year), int(month), int(day), int(hour), 
-        int(minute), int(second)).timestamp())
+        new_date_list.append(
+            datetime.datetime(
+                int(year), int(month), int(day), int(hour), int(minute), int(second)
+            ).timestamp()
+        )
 
     return new_date_list
 
-# only selecting data in LEO 
-dataset = dataset[dataset['SEMIMAJOR_AXIS'] < 8371]
-dataset['EPOCH'] = epoch(dataset['EPOCH'])
-dataset['tuples'] = [(0,0,0) for i in range(len(dataset.index))]
 
+# only selecting data in LEO
+dataset = dataset[dataset["SEMIMAJOR_AXIS"] < 8371]
+dataset["EPOCH"] = epoch(dataset["EPOCH"])
+dataset["tuples"] = [(0, 0, 0) for i in range(len(dataset.index))]
+dataset["SEMIMAJOR_AXIS"] = dataset["SEMIMAJOR_AXIS"].apply(
+    lambda x: x * 1000
+)  # Convert to meters
+
+matrices = []
+for index, row in dataset.iterrows():
+    R = Rotation.from_euler(
+        "zxz",
+        [-row["RA_OF_ASC_NODE"], -row["INCLINATION"], -row["ARG_OF_PERICENTER"]],
+        degrees=True,
+    )
+    matrices.append(R.as_matrix())
+
+dataset["rotation_matrix"] = matrices
 
 # Dataset to numpy array
 data_array = dataset.to_numpy()
-
-
-
-
