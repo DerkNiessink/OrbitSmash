6cf22dbe55e26674bc1bde9fcc3339feab9826a6
KatoSchmidt <70628138+KatoSchmidt@users.noreply.github.com>
2023-01-18 14:21:31 +0100

    data debris + group 21

--- sim/data_cleaning.py
+++ sim/data_cleaning.py
@@ -96,8 +96,15 @@ delet = list(group.loc[group == False].index)
 
 dataset = dataset[~dataset["groups"].isin(delet)]
 
+data_debris = dataset.loc[dataset['OBJECT_TYPE']  == 'DEBRIS']
+
 # Dataset to numpy array
 data_array = dataset.to_numpy()
+data_array_debris = data_debris.to_numpy()
 
 group_selection = data_array[:, 12] == 21
+group_selection_debris = data_array_debris[:, 12] == 21
+
 data_array_group21 = data_array[group_selection]
+data_array_debris_group21 = data_array_debris[group_selection_debris]
+
